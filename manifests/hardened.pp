3ef3d39e9fc1dff3179bfc68cfefe523d3ffabab
Matt Palmer <mpalmer@hezmatt.org>
2019-05-15 11:51:13 +1000

    Remove RIPE MD160 MACs
    They weren't real solid anyway, and newer SSH versions have dropped support
    for them entirely.

--- manifests/hardened.pp
+++ manifests/hardened.pp
@@ -17,7 +17,7 @@ class ssh::hardened {
 		# in an exact order, it's practically impossible to do via Augeas.  So,
 		# we do it the hard way
 		$cipher_list = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-ctr"
-		$mac_list    = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-ripemd160-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,hmac-ripemd160,umac-128@openssh.com"
+		$mac_list    = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com"
 		$kex_list    = "curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256"
 
 		exec {
